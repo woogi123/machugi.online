8eb7d7954c889a46729650c053989016d46e0379
woogi123 <07491@naver.com>
2025-04-09 17:36:01 +0900

    'start'

---
+++ src/main/java/machugi/online/example/machugi/MachugiApplication.java
@@ -0,0 +1,13 @@
+package machugi.online.example.machugi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachugiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachugiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/machugi/online/example/machugi/MachugiApplicationTests.java
@@ -0,0 +1,13 @@
+package machugi.online.example.machugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachugiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
